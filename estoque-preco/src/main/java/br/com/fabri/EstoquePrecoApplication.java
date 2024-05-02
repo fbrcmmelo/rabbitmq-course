165966ddc1301e5208559c22839f9b5468a163b8
fbrcmmelo <fbrcmmelo@gmail.com>
2024-05-01 21:54:08 -0300

    feat: created estoque-preco project will be producer as rest api

---
+++ estoque-preco/src/main/java/br/com/fabri/EstoquePrecoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoquePrecoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstoquePrecoApplication.class);
+    }
+
+}
